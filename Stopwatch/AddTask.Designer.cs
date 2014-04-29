ae8058b19e0b7696c95da518d2d5a1c3e6232d23
jth41 <thomas.holland.john@gmail.com>
2014-04-29 09:17:27 -0500

    I always forget to add the untracked files...

---
+++ Stopwatch/AddTask.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TaskStopwatch
+{
+    partial class AddTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minutesTextBox = new System.Windows.Forms.TextBox();
+            this.hoursTextBox = new System.Windows.Forms.TextBox();
+            this.taskTitleTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Minutes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Hours";
+            // 
+            // minutesTextBox
+            // 
+            this.minutesTextBox.Location = new System.Drawing.Point(100, 65);
+            this.minutesTextBox.Name = "minutesTextBox";
+            this.minutesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.minutesTextBox.TabIndex = 18;
+            this.minutesTextBox.Text = "0";
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.Location = new System.Drawing.Point(100, 38);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hoursTextBox.TabIndex = 17;
+            this.hoursTextBox.Text = "0";
+            // 
+            // taskTitleTextBox
+            // 
+            this.taskTitleTextBox.Location = new System.Drawing.Point(75, 12);
+            this.taskTitleTextBox.Name = "taskTitleTextBox";
+            this.taskTitleTextBox.Size = new System.Drawing.Size(205, 20);
+            this.taskTitleTextBox.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Task Title:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AddTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 152);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.minutesTextBox);
+            this.Controls.Add(this.hoursTextBox);
+            this.Controls.Add(this.taskTitleTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddTask";
+            this.Text = "AddTask";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox minutesTextBox;
+        private System.Windows.Forms.TextBox hoursTextBox;
+        private System.Windows.Forms.TextBox taskTitleTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
